01725f28190ed59d1f01829014f72327602ece61
SweedalMasih <sweedal.v.masih@gmail.com>
2025-03-05 07:15:51 +0530

    Episode-1: Inception

---
+++ App.js
@@ -0,0 +1,20 @@
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "h1- Hello World from nested elements!"),
+//     React.createElement("h2", {}, "h2- Hello World from nested elements!"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "h1- Hello World from nested elements!"),
+//     React.createElement("h2", {}, "h2- Hello World from nested elements!"),
+//   ]),
+// ]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World from React!"
+);
+console.log(heading); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
